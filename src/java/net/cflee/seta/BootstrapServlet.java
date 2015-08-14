41c335b38085aec9b3c44e41c0809eb832be90d3
Chiang Fong Lee <myself@cflee.net>
2015-08-14 15:21:29 +0800

    Initial commit

---
+++ src/java/net/cflee/seta/BootstrapServlet.java
@@ -0,0 +1,41 @@
+
+package net.cflee.seta;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "BootstrapServlet", urlPatterns = { "/admin/bootstrap" })
+public class BootstrapServlet extends HttpServlet {
+
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/jsp/admin-bootstrap.jsp").
+                forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+    }
+
+}
